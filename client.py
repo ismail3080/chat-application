8c98d1bdf7ad24311533a748f81f7c73c0e2ee89
ismail <83200379+ismail3080@users.noreply.github.com>
2022-10-30 14:00:11 +0100

    Add files via upload

---
+++ client.py
@@ -0,0 +1,15 @@
+STFUPSTFUPfrom multiprocessing.connection import Client
+import socket
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+Client.connect(("localhost", 9999))
+
+done = False
+while not done:
+    client.send(input("message").encode("utf-8"))
+    msg =-client.recv(1024).decode("utf-8")
+    if msg == "quit" :
+        done = True
+    else:
+        print(msg)
+    client.close()
